fb8c9232e0cae22801c6df9967d20eeb55ed3828
selametsamli <selamet96@gmail.com>
2020-03-20 16:48:26 +0300

    default slot ve slot defaults

--- Components/gelismis-component-kullanimi/src/App.vue
+++ Components/gelismis-component-kullanimi/src/App.vue
@@ -3,8 +3,8 @@
     <div class="col-md-6 col-md-offset-3">
 
       <app-memory>
-        <h1>Başlık</h1>
-        <p>Bu çok güzel bir anıydı</p>
+        <h1 slot="heading">Başlık</h1>
+        <p >Bu çok güzel bir anıydı</p>
       </app-memory>
 
     </div>
--- Components/gelismis-component-kullanimi/src/Memory.vue
+++ Components/gelismis-component-kullanimi/src/Memory.vue
@@ -1,19 +1,30 @@
 <template>
   <div>
-    <slot></slot>
+    <div class="memory">
+      <slot name="heading"></slot>
+    </div>
+    <div class="memory">
+      <slot></slot>
+    </div>
+    <div class="memory">
+      <slot name="author">
+        <h3>Author</h3>
+        <p>Buraya herhangi bir içerik gelmemiş</p>
+      </slot>
+    </div>
+
   </div>
 </template>
 
 
 <script>
-  export default {
-  }
+  export default {}
 
 </script>
 
 
 <style scoped>
-  div {
+  div.memory {
     border: 1px solid #ccc;
     box-shadow: 1px 1px 2px #666;
     padding: 20px 20px;
